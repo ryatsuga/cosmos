497ef5c6df59fad0445cf9ac9d8c3974da8cb0ef
Leandro <lf.passos11@gmail.com>
2019-10-02 19:06:25 -0300

    Update 2 01-10-19

---
+++ negocio/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	UpdateView, 
+	CreateView,
+	DeleteView,
+	TemplateView
+	)
