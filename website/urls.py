647a1cdfd65f63efe744ca119bf6b1f4440a8609
Leandro <lf.passos11@gmail.com>
2019-11-03 21:54:18 -0300

    Update 03-11-2019

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('services-list', views.services, name='services'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'website/home.html')
+
+def services(request):
+	return render(request, 'website/services.html')
